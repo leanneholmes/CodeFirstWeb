15d09b71cd9f09dae1b5e16eedda3dbd58841a95
Leanne Holmes <lholmes24@my.bcit.ca>
2023-01-20 15:56:55 -0800

    first commit

---
+++ Data/Migrations/20230120233208_Salary.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamPlayers.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Salary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Salary",
+                table: "Player",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Player",
+                keyColumn: "PlayerId",
+                keyValue: 1,
+                column: "Salary",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Player",
+                keyColumn: "PlayerId",
+                keyValue: 2,
+                column: "Salary",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Player",
+                keyColumn: "PlayerId",
+                keyValue: 3,
+                column: "Salary",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Player",
+                keyColumn: "PlayerId",
+                keyValue: 4,
+                column: "Salary",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salary",
+                table: "Player");
+        }
+    }
+}
